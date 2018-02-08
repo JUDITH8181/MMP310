0b8687aefb1a5ed04b1ea3888bc137a6367305d2
Judith Paredes <judith.paredes@f1006-21-34182.bmcc.cuny.edu>
2018-02-07 19:01:47 -0500

    updated input

--- input/app.js
+++ input/app.js
@@ -6,7 +6,7 @@ const boroughSelect= document.getElementById("borough");
 
 submitButton.onclick = function(event)
 {
-    const messgae= document.createElement("p");
+    const message = document.createElement("p");
     message.textContent = "Hello, " + nameInput.value+ boroughSelect.value+ " welcome to my website.";
-    document.body.appendChild(messgae);
+    document.body.appendChild(message);
 }
